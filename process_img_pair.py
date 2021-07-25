a2634bbb80dcf5ef2e548d3bca6a04ed912d37fd
andytorrestb <andytorrestb@gmail.com>
2021-07-25 07:00:05 +0000

    add batch processing for images

---
+++ process_img_pair.py
@@ -0,0 +1,74 @@
+from openpiv import tools, pyprocess, validation, filters, scaling
+import numpy as np
+
+def process_img_pair( args ):
+  """
+      Custom function used to calculate a flowfield from an image pair.
+      It is fed to the Multiprocessor object and produces flowfields in batches.
+  """
+
+  # Unpack arguments. All are required.
+  file_a, file_b, counter = args
+
+  # print(file_a, file_b, counter)
+
+  path , seperator , name = file_a.rpartition('/')
+  # print(path , seperator , name)
+
+  # read images into numpy arrays
+  frame_a = tools.imread( file_a )
+  frame_b = tools.imread( file_b )
+
+  # Search parameters
+  winsize = 32 # pixels, interrogation window size in frame A
+  searchsize = 38  # pixels, search area size in frame B
+  overlap = 17 # pixels, 50% overlap
+  dt = 2e-5 # sec, time interval between the two frames
+
+  # # --- Process Data ---
+  u0, v0, sig2noise = pyprocess.extended_search_area_piv(
+    frame_a.astype(np.int32),
+    frame_b.astype(np.int32),
+    window_size=winsize,
+    overlap=overlap,
+    dt=dt,
+    search_area_size=searchsize,
+    sig2noise_method='peak2peak',
+  )
+  
+  u1, v1, mask = validation.sig2noise_val(
+    u0, v0,
+    sig2noise,
+    threshold = 1.05,
+    )
+  
+  u2, v2 = filters.replace_outliers(
+    u1, v1,
+    method='localmean',
+    max_iter=3,
+    kernel_size=3,
+  )
+
+  # get window centers coordinates
+  x, y = pyprocess.get_coordinates(
+    image_size=frame_a.shape,
+    search_area_size=searchsize,
+    overlap=overlap,
+  )
+
+  scaling_factor = 5
+  x, y, u3, v3 = scaling.uniform(
+      x, y, u2, v2,
+      scaling_factor = scaling_factor,  # 96.52 pixels/millimeter
+  )
+
+
+  # 0,0 shall be bottom left, positive rotation rate is counterclockwise
+  x, y, u3, v3 = tools.transform_coordinates(x, y, u3, v3)  
+  # save to a file
+
+  case_name, f_num, letter, ext = name.split('.')
+
+  filename = path.rpartition('/')[0] + '/results/' + case_name + '.' + f_num + '.' + 'txt'
+
+  tools.save(x, y, u3, v3, mask, filename)
--- process_piv.py
+++ process_piv.py
@@ -1,8 +1,12 @@
+from openpiv import tools, pyprocess, validation, filters, scaling
+
 
 import cv2, os, sys
 
 import extract_frames as ef 
 import rename_images as ri
+import process_img_pair as piv
+
 
 def main():
   # Check for valid input
@@ -12,11 +16,30 @@ def main():
   # Extract frames from video file
   if (len(sys.argv) == 2):
         file_name = sys.argv[1]
-        img_dir = ef.extract_frames(file_name)
+        input_dir = ef.extract_frames(file_name)
 
-  # Rename images accorind to a-b pairs
-  ri.rename_images(img_dir)
+  # Rename images accoriding to a-b pairs
+  ri.rename_images(input_dir)
   
+  file_name = os.listdir(input_dir)[0]
+
+  name, number, frame, ext = file_name.split('.')
+  print(name, number, frame, ext)
+  # Process images using OpenPIV
+  pattern_a = name + '.*.a.' + ext
+  pattern_b = name + '.*.b.' + ext
+
+  results_dir = input_dir.rpartition('/')[0]
+  results_dir = results_dir.rpartition('/')[0] + '/results'
+  os.mkdir(results_dir)
+
+  task = tools.Multiprocesser(
+      data_dir = input_dir,
+      pattern_a= pattern_a,
+      pattern_b=pattern_b
+      )
+
+  task.run( func = piv.process_img_pair, n_cpus=8 )
 
 
 main()
