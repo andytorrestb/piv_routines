db4b11e340932d6a0da54290c1333218ff52abcc
andytorrestb <andytorrestb@gmail.com>
2021-08-13 16:29:09 +0000

    add congif file

---
+++ piv_config.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import preprocessing
+
+basic_params = {
+  'winsize': 32,
+  'searchsize': 38,
+  'overlap': 17,
+  'dt': 2e-5
+}
+
+validation_params = {
+  'using': True,
+  'threshold': 1.05
+}
+
+outlier_params = {
+  'using': True,
+  'method': 'test',
+  'max_iter': 3,
+  'kernel_size': 3
+}
+
+scaling_params = {
+  'using': True,
+  'scale': 5
+}
+
+params = {
+  'basic': basic_params,
+  'validation': validation_params,
+  'outliers': outlier_params,
+  'scale': scaling_params
+}
+
+use_anonymous = True
