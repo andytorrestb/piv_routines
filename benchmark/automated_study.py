de010d86cd189cbf5b40e1acd5c1e9ac83684c20
andytorrestb <andytorrestb@gmail.com>
2023-02-18 07:08:49 -0500

    establish skeleton for automated_study.py

---
+++ benchmark/automated_study.py
@@ -0,0 +1,44 @@
+# Andy Torres
+# Undergraduate Research Assistant
+# Computational Fluids and Aerodynamics Laboratory
+# University of Central Florida
+
+# ===================================
+# PIV_Routines: automated_study.py ||
+# ===================================
+
+'''
+Example case to outline the process of benchmarking a PIV analysis. 
+This example involves generating synthetic data. Two images are generated, 
+where the particles move within the image from frame a to b according to some 
+vector field deemed the ground truth. In theory, this should be the 'cleanest' way
+to prototype this sort of process. 
+'''
+
+# =================================================================================
+# ||                         Section 0: Configure Logger                         ||
+# =================================================================================
+
+# =================================================================================
+# ||                      Section 1: Generate Synthetic Data                     ||
+# =================================================================================
+
+# =================================================================================
+# ||                           Section 2: Process Images                         ||
+# =================================================================================
+
+# =================================================================================
+# ||                       Section 3: Post-Process PIV Results                   ||
+# =================================================================================
+
+# =================================================================================
+# ||                       Section 4: Data Cleaning of Results                   ||
+# =================================================================================
+
+# =================================================================================
+# ||                    Section 5: Comparative Analysis of Results               ||
+# =================================================================================
+
+# =================================================================================
+# ||                   Section 6: Benchmarking Analysis of Results               ||
+# =================================================================================
